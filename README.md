f1b7f03eb7542346a0350bdfb212bbbaa1b12952
Gary A. Stafford <garystafford@rochester.rr.com>
2021-03-14 20:24:32 -0400

    Initial commit of public version of project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Amazon Macie Demonstration
+
+## Background
+
+Source code files for a demonstration of Amazon Macie, part of the upcoming post, 'Data Lakes: Discovery, Security, and Privacy of Sensitive Data: Employing Amazon Macie to discover and protect sensitive data in your Amazon S3-based Data Lake'.
+
+## Workflow Architecture
+
+![Workflow Architecture](./diagram/Macie.png)
---
+++ scripts/create_macie_job_daily.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# Purpose: Create Daily Macie classification job - Synthea patient data
+# Author:  Gary A. Stafford (March 2021)
+
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s - %(message)s', level=logging.INFO)
+
+ssm_client = boto3.client('ssm')
+sts_client = boto3.client('sts')
+macie_client = boto3.client('macie2')
+
+
+def main():
+    params = get_parameters()
+    account_id = sts_client.get_caller_identity()['Account']
+    custom_data_identifiers = list_custom_data_identifiers()
+    create_classification_job(params['patient_data_bucket'], account_id, custom_data_identifiers)
+
+
+def list_custom_data_identifiers():
+    """Returns a list of all custom data identifier ids"""
+
+    custom_data_identifiers = []
+
+    try:
+        response = macie_client.list_custom_data_identifiers()
+        for item in response['items']:
+            custom_data_identifiers.append(item['id'])
+        return custom_data_identifiers
+    except ClientError as e:
+        logging.error(e)
+    finally:
+        return custom_data_identifiers
+
+
+def create_classification_job(patient_data_bucket, account_id, custom_data_identifiers):
+    """Create Daily Macie classification job"""
+
+    try:
+        response = macie_client.create_classification_job(
+            customDataIdentifierIds=custom_data_identifiers,
+            description='Review Synthea patient data (Daily)',
+            jobType='SCHEDULED',
+            initialRun=True,
+            name='SyntheaPatientData_Daily',
+            s3JobDefinition={
+                'bucketDefinitions': [
+                    {
+                        'accountId': account_id,
+                        'buckets': [
+                            patient_data_bucket
+                        ]
+                    }
+                ]
+            },
+            samplingPercentage=100,
+            scheduleFrequency={
+                'dailySchedule': {}
+            },
+            tags={
+                'Project': 'Amazon Macie Demo'
+            }
+        )
+        print(f'Response: {response}')
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def get_parameters():
+    """Load parameter values from AWS Systems Manager (SSM) Parameter Store"""
+
+    params = {
+        'patient_data_bucket': ssm_client.get_parameter(Name='/macie_demo/patient_data_bucket')['Parameter']['Value']
+    }
+
+    return params
+
+
+if __name__ == '__main__':
+    main()
